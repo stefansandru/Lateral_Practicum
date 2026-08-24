f81c986571457b63f40750222c7f9b86498c6a79
stefansandru <stefansandru04@gmail.com>
2026-08-24 18:31:32 +0300

    Revert "Merge commit '54dfefab5e95e04ac838663a69176f94216f213e' as 'Final_App'"
    This reverts commit 3462dbbf3063f9aa5dc7a98063d0fe7ccc8b8b95, reversing
    changes made to 6e009920560d7c2c27b9b3e7ce5dcbbc81f98bdc.

--- Final_App/docker_build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker build --platform linux/amd64 --compress -t stef04/plant-care-card:latest .
--- Final_App/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /
-gunicorn -w ${WORKERS:=2} \
-  -b :80 -t ${TIMEOUT:=300} \
-  -k uvicorn.workers.UvicornWorker \
-  app.main:app
